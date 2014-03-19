06eacdbcaee0bba328e51d3bce24e6bb2bb00ffc
Ricardo <ricardo@vaiolinux.(none)>
2014-03-19 18:32:27 +0000

    V3

---
+++ html/ecra.js
@@ -0,0 +1,101 @@
+function init(){
+	var xMax = 40;
+	var yMax = 24;
+	var larg = 25;
+	var f = new Array();
+	var boxes = new Array();
+	var socket = io.connect();
+
+	var c = document.getElementById("myCanvas");
+	var ctx = c.getContext("2d");
+
+	socket.on("connect", function(data) {
+		geraArray();
+	});
+
+	function geraArray() {
+		for (var x = 0; x < xMax; x++) {
+			f[x] = new Array();
+			for (var y = 0; y < yMax; y++) {
+				f[x][y] = 0;
+				//alert(f[x][y]);
+			}
+		}
+	}
+
+
+	socket.on('desenha', function(dados) {
+
+		var rect = new Box(dados.id, dados.x, dados.y, larg, larg, "green");
+		boxes.push(rect);
+		geraQuad(dados.x, dados.y, larg, larg, "green");
+	});
+
+	function Box(id, x, y, w, h, fill) {
+		this.id = id;
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		this.fill = fill;
+	}
+
+	function geraQuad(x, y, w, h, fill) {
+		f[x][y] = 1;
+		//alert(f[x][y]);
+		x = x * larg;
+		y = y * larg;
+		ctx.fillStyle = fill;
+		ctx.fillRect(x, y, w, h);
+	};
+
+	function clearCanvas(x, y, w, h) {
+		f[x][y] = 0;
+		//c.width = c.width;
+		x = x * larg;
+		y = y * larg;
+		ctx.clearRect(x, y, larg, larg);
+	}
+
+
+	socket.on('teste', function(msg) {
+		console.log(msg);
+	});
+
+	// {"comando":"up","id",3}
+	socket.on('dados', function(msg) {
+		//alert(boxes[msg.id].x);
+		clearCanvas(boxes[msg.id].x, boxes[msg.id].y, boxes[msg.id].w, boxes[msg.id].h);
+		switch(msg.comando) {
+			case"up":
+				if (boxes[msg.id].y > 0) {
+					if (f[boxes[msg.id].x][boxes[msg.id].y - 1] == 0) {
+						boxes[msg.id].y--;
+					}
+				}
+				break;
+			case"left":
+				if (boxes[msg.id].x > 0) {
+					if (f[boxes[msg.id].x - 1][boxes[msg.id].y] == 0) {
+						boxes[msg.id].x--;
+					}
+				}
+				break;
+			case"down":
+				if (boxes[msg.id].y < yMax) {
+					if (f[boxes[msg.id].x][boxes[msg.id].y + 1] == 0) {
+						boxes[msg.id].y++;
+					}
+				}
+				break;
+			case"right":
+				if (boxes[msg.id].x < xMax - 1) {
+					if (f[boxes[msg.id].x + 1][boxes[msg.id].y] == 0) {
+						boxes[msg.id].x++;
+					}
+				}
+				break;
+		}
+		geraQuad(boxes[msg.id].x, boxes[msg.id].y, boxes[msg.id].w, boxes[msg.id].h, boxes[msg.id].fill);
+	});
+}
--- servidor.js
+++ servidor.js
@@ -1,81 +1,90 @@
-/**
- * "Client Counter" node.js
- * 
- * @author Pedro Moreira <pmoreira@estg.ipvc.pt> 2014.03.04
- * @version 0.1
- * 
- */
+var http = require('http'),
+static = require('node-static'),
+io = require('socket.io');
 
-
-// modulo com servidor http
-var 	http 		= require('http'),			//  http server
-		static		= require('node-static'),	//  static server
-		io			= require('socket.io');
-		
 var server = http.createServer(handler);
 
-// new fileserver for static content
-// static content is at the 'static' directory
-// notice: no need to prepend the full path to serve the file
 var fileServer = new static.Server('./html');
 
 function handler(request, response) {
-	    request.addListener('end', function () {
-			// if no filepath, serves cvisual.html by default
-		    console.log("----"+request.url);
-	        fileServer.serve(request, response, function (e, resp) {
-				var filePath = request.url;
-				console.log("==="+filePath);
-				if (e && e.status == 404) {
-					if (filePath == '/') {
-						httpcode = 200;
-						filePath = 'blocos.html';
-					} else {
-						httpcode = 404;
-						filePath = 'errorWorld.html';
-					}
+	request.addListener('end', function() {
+		// if no filepath, serves cvisual.html by default
+		console.log("----" + request.url);
+		fileServer.serve(request, response, function(e, resp) {
+			var filePath = request.url;
+			console.log("===" + filePath);
+			if (e && e.status == 404) {
+				if (filePath == '/') {
+					httpcode = 200;
+					filePath = 'cliente.html';
+				} else if (filePath == '/servidor') {
+					httpcode = 200;
+					filePath = 'ecra.html';
+				} else {
+					httpcode = 404;
+					filePath = 'errorWorld.html';
 				}
-                fileServer.serveFile(filePath, httpcode, {}, request, response);
-				});
-	    });
-request.resume();
+			}
+			fileServer.serveFile(filePath, httpcode, {}, request, response);
+		});
+	});
+	request.resume();
 }
 
 // to manage connected clients
+
 var connectedClients = new Array();
-var id=0;
+var id = 0;
+var x = 0;
+var y = 0;
 
 // attach a socketio interface to the server
 var socketio = io.listen(server);
 
 socketio.sockets.on('connection', function(client) {
-  	if (client.send) {console.log('ok');} else {console.log('nok');}
-  
-  	connectedClients.push(client);
-  	count = connectedClients.length;
-  	
-  	client.emit("logged",{"id":id});
-  	id++;
-   // emit a count message to all
-   
-   socketio.sockets.emit('count',{"count":count});
-      
-   // on disconnect from any client
-   // updates connectedClients and sends count message
-   client.on('disconnect', function () {
-		connectedClients.splice(connectedClients.indexOf(client), 1);
-        count = connectedClients.length;
-        socketio.sockets.emit('count', {"count": count});
-    });
-    
-    client.on("click", function(data) {
-    	socketio.sockets.emit("click",data);
-    });
-           
-   return;
+	connectedClients.push(client);
+	count = connectedClients.length;
 
-  });
+	client.emit("logged", {"id":id, "x":x, "y":y});
+	id++;
+	x++;
+	y++;
+	socketio.sockets.emit('desenha', {"id":id, "x":x, "y":y});
+	client.on('disconnect', function() {
+		connectedClients.splice(connectedClients.indexOf(client), 1);
+		count = connectedClients.length;
+	});
+
+	client.on("click", function(data) {
+		var comando;
+		var id = data.id;
+		var code = data.code;
+		switch(code) {
+			case 83:
+				comando = "down";
+				break;
+			case 87:
+				comando = "up";
+				break;
+			case 68:
+				comando = "right";
+				break;
+			case 65:
+				comando = "left";
+				break;
+		}
+		msg = {
+			"comando" : comando,
+			"id" : id
+		};
+		//mKeyP(msg);
+		socketio.sockets.emit('dados', {"comando":comando, "id":id});
+		//socketio.sockets.emit("click",data);
+	});
+
+	return;
+
+});
 
 var port = process.env.PORT || 5000;
 server.listen(port);
-
